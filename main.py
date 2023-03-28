bb3d29dd962bd7e9173f5edfde14ac897e130bd3
Кулагин Андрей Андреевич <s121917@guu.ru>
2023-03-28 18:49:40 +0300

    q

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from selenium.webdriver.chrome.options import Options
 def get_card_color_info(driver, data, is_sold_out):
     while True:
         try:
-            print("загружаем страницу после клика")
             images_block = driver.find_element(By.ID, "experience-wrapper")
-            print("загрузили")
             break
         except Exception as e:
             if "SBOX_FATAL_MEMORY_EXCEEDED" in str(e):
@@ -35,10 +33,8 @@ def get_card_color_info(driver, data, is_sold_out):
     price = model_sub_name_block.find_element(By.CSS_SELECTOR,
                                               ".product-price").text
     data["price"] = price
-    # print(price)
-    coming_soon = driver.find_elements(By.CSS_SELECTOR, 'data-test="comingSoon"')
-    is_coming_soon = len(coming_soon)
-    if not is_sold_out and not is_coming_soon:
+
+    if not is_sold_out and not is_coming_soon(driver):
         sizes_block = driver.find_element(By.CSS_SELECTOR, ".mt5-sm.mb3-sm.body-2")
         active_sizes_inputs = sizes_block.find_elements(By.CSS_SELECTOR, "input:not([disabled])")
         sizes = []
@@ -52,16 +48,23 @@ def get_card_color_info(driver, data, is_sold_out):
         data["size"] = sizes
 
 
+def is_coming_soon(driver):
+    coming_soon = driver.find_elements(By.CLASS_NAME, 'sold-out')
+    is_coming_soon_eq = len(coming_soon)
+    coming_soon = driver.find_elements(By.CSS_SELECTOR,
+                                       '.bg-primary-grey.ta-sm-c.mr1-md.ml0-md.mr4-sm.ml4-sm.pt4-sm.pb4-sm.mt5-md.mt8-sm.mb4-sm.mb4-md')
+    is_coming_soon_eq += len(coming_soon)
+    return is_coming_soon_eq
+
+
 def cookie_allow(driver):
     card_link = "https://www.nike.com/t/air-jordan-1-mid-se-mens-shoes-Zn07hL/DV1308-104"
     driver.get(card_link)
     while True:
         try:
-            print("пробуем кликнуть")
             time.sleep(3)
             element = driver.find_element(By.CLASS_NAME, "nds-dialog__actions")
             element.click()
-            print("кликнули")
             break
         except:
             driver.refresh()
@@ -87,10 +90,15 @@ def get_card_info(driver, card_link, card_data):
         driver.get(base_link + article["article"])
         data["article"] = article["article"]
         # print(article)
-        print(f"{article['article']}", article["article"])
         get_card_color_info(driver, data, article["is_sold_out"])
-        print(f"{article['article']}", article["article"])
         card_data[f"{article['article']}"] = dict(data)
+
+
+def write_to_file(card_data):
+    with open("output.txt", 'a') as card_links:
+        for data in card_data:
+            card_links.write(str(card_data[data])+'\n')
+
 
 
 # Словарь с данными по карточке
@@ -102,31 +110,13 @@ chrome_options.add_experimental_option("useAutomationExtension", False)
 # chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 cookie_allow(driver)
-'''"https://www.nike.com/t/air-jordan-1-mid-se-mens-shoes-Zn07hL/DV1308-104",
-    "https://www.nike.com/t/air-jordan-6-retro-mens-shoes-CVPFVM/CT8529-100",
-    "https://www.nike.com/t/jordan-delta-2-se-shoes-ZpvFm3/DJ9843-004",
-    "https://www.nike.com/t/air-jordan-1-low-mens-shoes-0LXhbn/553558-215",
-    "https://www.nike.com/t/air-jordan-1-mid-mens-shoes-b3js2D/DQ8426-215",
-    "https://www.nike.com/t/air-jordan-1-mid-se-mens-shoes-Zn07hL/DV1308-104",
-    "https://www.nike.com/t/air-jordan-1-zoom-cmft-2-mens-shoes-Tw02qw/DV1307-060",
-    "https://www.nike.com/t/air-jordan-7-retro-mens-shoes-098sXt/CU9307-106",
-    "https://www.nike.com/t/air-jordan-7-retro-se-mens-shoes-8mRqbR/DN9782-001",
-    "https://www.nike.com/t/air-jordan-xxxvii-low-basketball-shoes-00ZHpg/DV9909-401",
-    "https://www.nike.com/t/air-jordan-11-retro-low-mens-shoes-4kj41D/AV2187-140",
-    "https://www.nike.com/t/jordan-jumpman-team-ii-mens-shoe-GrWxfR/819175-106",
-    "https://www.nike.com/t/air-jordan-1-low-se-mens-shoes-kMt2HL/DQ8422-001",
-    "https://www.nike.com/t/air-jordan-1-retro-high-og-mens-shoes-VdpsB7/DZ5485-303",
-    "https://www.nike.com/t/luka-1-next-nature-basketball-shoes-69X9Vs/DN1772-108",'''
-card_links = [
-    "https://www.nike.com/t/air-jordan-11-retro-low-mens-shoes-4kj41D/AV2187-140"
-
-]
-for card_link in card_links:
-    print(card_link)
-    card_data = {}
-    get_card_info(driver, card_link, card_data)
-    print(card_data)
-    print(card_link, '\n\n')
+with open("links.txt", 'r') as card_links:
+    for card_link in card_links:
+        print(card_link)
+        card_data = {}
+        get_card_info(driver, card_link, card_data)
+        print(card_data)
+        write_to_file(card_data)
 
 driver.quit()
-driver.close()
+
--- main2.py
+++ main2.py
@@ -4,148 +4,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
-def get_phone_number(contact_block):
-    try:
-        phone_block = contact_block.find_element(By.CLASS_NAME, "_3ea6fa5da8--phones-container--g5vgs")
-        touch_phone_block = phone_block.find_element(By.CLASS_NAME, "_3ea6fa5da8--phones_minimized--XieZH")
-        touch_phone_block.click()
-        phone_html = touch_phone_block.get_attribute("outerHTML")
-        phone = phone_html[phone_html.find("tel:") + 4:]
-        phone = phone[:phone.find('"')]
-        print(phone)
-        return phone
-    except:
-        print("Нет телефона")
-        return "Нет телефона"
-
-
-def get_email(contact_block):
-    try:
-        email_blocks = contact_block.find_elements(By.CLASS_NAME, "_3ea6fa5da8--socnetwork--Q6ec4")
-        if len(email_blocks) > 1:
-            for el in email_blocks:
-                link_block = el.find_element(By.CLASS_NAME, "_3ea6fa5da8--socnetwork-icon--aUE0p")
-                email_html = link_block.get_attribute("outerHTML")
-                if email_html.find("mailto:") != -1:
-                    email_block = el
-                    break
-        else:
-            email_block = email_blocks[0]
-        link_block = email_block.find_element(By.CLASS_NAME, "_3ea6fa5da8--socnetwork-icon--aUE0p")
-        email_html = link_block.get_attribute("outerHTML")
-        email = email_html[email_html.find("mailto:") + 7:]
-        email = email[:email.find('"')]
-        print(email)
-        return email
-    except:
-        print("Нет email")
-        return "Нет email"
-
-
-def get_fio(element2):
-    name_block = element2.find_element(By.CLASS_NAME, "_3ea6fa5da8--title--OdkyF")
-    name_block = name_block.find_element(By.CLASS_NAME, "_3ea6fa5da8--agent-name--Q6y1w")
-    name_block = name_block.find_element(By.CLASS_NAME, "_3ea6fa5da8--name--JPPsh")
-    print(name_block.text)
-    return name_block.text
-
-
-def get_agency(element2):
-    information_blocks = element2.find_elements(By.CLASS_NAME, "_3ea6fa5da8--row--eMUih")
-    agency_block = []
-    for el in information_blocks:
+def cookie_allow(driver):
+    card_link = "https://www.nike.com/t/air-jordan-1-mid-se-mens-shoes-Zn07hL/DV1308-104"
+    driver.get(card_link)
+    while True:
         try:
-            if el.find_element(By.CLASS_NAME, "_3ea6fa5da8--about-title--OCzbj").text == "Агентство":
-                agency_block = el.find_element(By.CLASS_NAME, "_3ea6fa5da8--about-text--xx5UG").text
-                break
-            else:
-                agency_block = "Нет информации об агентстве"
+            print("пробуем кликнуть")
+            time.sleep(3)
+            element = driver.find_element(By.CLASS_NAME, "nds-dialog__actions")
+            element.click()
+            print("кликнули")
+            break
         except:
-            continue
-    print(agency_block)
-    return agency_block
-
-
-def get_region(element2):
-    agency_information_block = element2.find_elements(By.CLASS_NAME, "_3ea6fa5da8--row--eMUih")
-    region_block = []
-    for el in agency_information_block:
-        try:
-            if el.find_element(By.CLASS_NAME, "_3ea6fa5da8--about-title--OCzbj").text == "Регион работы":
-                region_block = el.find_element(By.CLASS_NAME, "_3ea6fa5da8--about-text--xx5UG").text
-                break
-            else:
-                region_block = "Нет информации о регионе"
-        except:
-            continue
-    print(region_block)
-    return region_block
-
-
-def get_experience(element2):
-    experience_block = element2.find_element(By.CLASS_NAME, "_3ea6fa5da8--counters--YH7j7")
-    year_experience_block = experience_block.find_element(By.CLASS_NAME, "_3ea6fa5da8--counters-item--kwGtk")
-    year_experience_html = year_experience_block.get_attribute("outerHTML")
-    position = year_experience_html.find("с 2")
-    if position == -1:
-        position = year_experience_html.find("с 1")
-    year_experience = year_experience_html[position:position + 6]
-    print(year_experience)
-    return year_experience
-
-
-def get_rating(element2):
-    rating_bloke = element2.find_element(By.CLASS_NAME, "_3ea6fa5da8--rating-desctiption--HgRir")
-    print(rating_bloke.text)
-    return rating_bloke.text
-
-
-def get_objects_in_work(element2):
-    experience_block = element2.find_element(By.CLASS_NAME, "_3ea6fa5da8--counters--YH7j7")
-    in_work_block = experience_block.find_elements(By.CLASS_NAME, "_3ea6fa5da8--counters-item--kwGtk")
-    for el in in_work_block:
-        text = el.text
-        if text.find("В работе") != -1:
-            out_str = text[text.find("В работе") + 9:]
-        else:
-            out_str = "Нет информации об объектах в работе"
-    return out_str
-
-
-def main_func(driver):
-    out_str = ""
-    element = driver.find_element(By.CLASS_NAME, "_3ea6fa5da8--container--bqTAG")
-    element1 = element.find_element(By.CLASS_NAME, "_3ea6fa5da8--content--Q3iIo")
-    element2 = element1.find_element(By.CLASS_NAME, "_3ea6fa5da8--main--xPGzo")
-    fio = get_fio(element2)
-    agency = get_agency(element2)
-    region = get_region(element2)
-    experience = get_experience(element2)
-    rating = get_rating(element2)
-    objects_in_work = get_objects_in_work(element2)
-    # здесь сделать трай эксепт на нет контактов
-    contact_block = element2.find_element(By.CLASS_NAME, "_3ea6fa5da8--container--j6vKU")
-    phone_number = get_phone_number(contact_block)
-    email = get_email(contact_block)
-    print()
-    cure_link = driver.current_url
-    out_str = f"{fio};{email};{phone_number};{agency};{region};{experience};{rating};{objects_in_work};{cure_link}\n"
-    return out_str
-
-
-def get_pages_count(driver, link):
-    driver.get(link)
-    page_count = 1
-    try:
-        pages_count_block = driver.find_elements(By.CLASS_NAME, "_9400a595a7--content--sGuO7")
-        for el in pages_count_block:
-            page_count = el.text
-        try:
-            return int(page_count)
-        except:
-            print("WARNING!!! Получено не числовое значение кол-ва страниц")
-    except:
-        return page_count
+            driver.refresh()
 
 
 chrome_options = Options()
@@ -153,31 +24,36 @@ chrome_options = Options()
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 chrome_options.add_experimental_option("useAutomationExtension", False)
 # chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(executable_path="/home/andrey/PycharmProjects/parser_cian_1/chromedriver",
-                          options=chrome_options)
-regions_links = open("regions_links.txt", "r")
+driver = webdriver.Chrome(options=chrome_options)
+cookie_allow(driver)
+links = [
+    "https://www.nike.com/w/mens-jordan-shoes-37eefznik1zy7ok"
+]
+for link in links:
+    print(link)
+    driver.get(link)
+    driver.set_window_size(1920, 1080)
 
-for link in regions_links:
-    link_pattern = link[:link.find("page=") + 5]
-    pages_count = get_pages_count(driver, link)
-    print(link_pattern)
-    for page_num in range(1, pages_count):
-        print(page_num)
-        cure_link = link_pattern + str(page_num)
-        driver.get(cure_link)
-        realtor_objects = driver.find_elements(By.CLASS_NAME, "_9400a595a7--container--J25nK")
-        for realtor in realtor_objects:
-            realtor.click()
-            driver.switch_to.window(driver.window_handles[1])
-            try:
-                out_str = main_func(driver)
-                output_file = open("output.txt", "a")
-                output_file.write(out_str)
-                output_file.close()
-            except:
-                pass
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    # Проматывание страницы до самого конца
+    cards = 0
+    while True:
+        # Cкроллим до конца страницы
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-driver.close()
-regions_links.close()
+        # Ждем, пока страница загрузится
+        time.sleep(3)
+
+        # Вычисляем высоту страницы после скролла
+        cards = driver.find_elements(By.CLASS_NAME, "product-card__img-link-overlay")
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        count = driver.find_element(By.XPATH, "//*[@id='Mens-Jordan-Shoes']/span").text
+        if len(cards) == int(count.strip('()')):
+            break
+        last_height = new_height
+    cards = driver.find_elements(By.CLASS_NAME, "product-card__img-link-overlay")
+    print(len(cards))
+    f = open("links.txt", 'a')
+    for card in cards:
+        f.write(card.get_attribute('href')+'\n')
+        print(card.get_attribute('href'))
